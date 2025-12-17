6a8ed166af1a23a532b447da1bc6dc7d61e01a6f
gcolomer <gcolomer@protonmail.com>
2025-12-17 09:31:34 +0100

    Enhance Snake class with direction handling and body management
    - Introduced direction attribute to manage snake movement direction.
    - Updated body attribute type to use Vector2 for improved position handling.
    - Refactored move and eat methods to accommodate growth effects and streamline body updates.
    - Added methods to retrieve head and body positions for better access during gameplay.

--- src/snake.py
+++ src/snake.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.math import Vector2
+from enum import Enum
 from src.config import SNAKE_HEAD, SNAKE_BODY, EMPTY_CASE, CELL_SIZE
 from src.brain import Brain
 from src.object import Object
@@ -7,13 +8,15 @@ from src.utils import get_random_position
 
 
 class Snake(Object):
+
     def __init__(self, board: list[list[int]]) -> None:
         self.head_id = SNAKE_HEAD
         self.body_id = SNAKE_BODY
         self.brain = Brain(board.shape)
+        self.direction = Vector2(1, 0)
 
         x, y = get_random_position(board)
-        self.body: list[tuple[int, int]] = [Vector2(x, y)]
+        self.body: list[Vector2] = [Vector2(x, y)]
         for _ in range(2):
             last_pos = self.body[-1]
             for action in self.brain.actions:
@@ -30,6 +33,10 @@ class Snake(Object):
         if self.get_length() != 3:
             print(self.body)
             raise ValueError("The snake is too short... No place available")
+        
+        for i, snake_piece in enumerate(self.body):
+            id = SNAKE_HEAD if i == 0 else SNAKE_BODY
+            board[int(snake_piece.x)][int(snake_piece.y)] = id
 
     def draw(self, screen):
         for i, pos in enumerate(self.body):
@@ -46,7 +53,10 @@ class Snake(Object):
             pygame.draw.rect(screen, color, rect)
 
     def get_position(self):
-        return int(self.body[0].x), int(self.body[0].y)
+        return self.body
+
+    def get_head_position(self):
+        return self.body[0]
 
     def get_body_position(self):
         return self.body[1:]
@@ -54,17 +64,17 @@ class Snake(Object):
     def get_length(self):
         return len(self.body)
 
-    def move(self, action):
-        body_copy = self.body[:-1]
-        body_copy.insert(0, body_copy[0] + action)
+    def move(self, action: Vector2, growth_effect: int = -1):
+        self.direction = action
+        if growth_effect == 0:
+            growth_effect = len(self.body)
+        body_copy = self.body[:growth_effect]
+        body_copy.insert(0, body_copy[0] + self.direction)
         self.body = body_copy[:]
 
-    # TODO: Comment ralonger la queue du serpent ?
     def eat(self, nutrient: int = 0):
-        if nutrient == 1:
-            self.body.insert(1, tuple(self.head_pos))
-        else:
-            self.body.pop()
+        print(f"Snack {nutrient}")
+        self.move(self.direction, -1 + nutrient)
 
     def watch(self, board):
         head_x, head_y = self.get_position()
